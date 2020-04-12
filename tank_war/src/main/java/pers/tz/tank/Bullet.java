bc5c29d383cdc25c12d9fd56e445adbc263eb449
twz <1151207663@qq.com>
2020-04-12 17:21:39 +0800

    增加子弹图片，以及子弹图片的方向

--- tank_war/src/main/java/pers/tz/tank/Bullet.java
+++ tank_war/src/main/java/pers/tz/tank/Bullet.java
@@ -27,10 +27,20 @@ public class Bullet {
 	public void paint(Graphics g) {
 		if( !living ) tf.bullets.remove(this);
 		
-		Color c = g.getColor();
-		g.setColor(Color.RED);
-		g.fillOval(x, y, 30, 30);
-		g.setColor(c);
+		switch(dir) {
+			case LEFT:
+				g.drawImage(ResourceMgr.bulletL, x, y, null);
+				break;
+			case RIGHT:
+				g.drawImage(ResourceMgr.bulletR, x, y, null);
+				break;
+			case UP:
+				g.drawImage(ResourceMgr.bulletU, x, y, null);
+				break;
+			case DOWN:
+				g.drawImage(ResourceMgr.bulletD, x, y, null);
+				break;
+		}
 		
 		move();
 	}
