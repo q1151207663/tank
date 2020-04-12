0e40262fd19311a9e18fa5e167f99f86de452118
twz <1151207663@qq.com>
2020-04-12 17:03:45 +0800

    添加Junit5组件

---
+++ tank_war/src/test/java/pers/tz/test/ImageTest.java
@@ -0,0 +1,30 @@
+package pers.tz.test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class ImageTest {
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		try {
+			BufferedImage image = ImageIO.read(new File("D:/学习相关/tank war静态资源/资源/images/BadTank2.png"));
+			assertNotNull(image);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
