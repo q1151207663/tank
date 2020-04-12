b23959f3b7813773c4efbaef65e92a27c5922647
twz <1151207663@qq.com>
2020-04-12 15:58:03 +0800

    用双缓冲解决子弹闪烁问题

--- tank_war/src/main/java/pers/tz/tank/TankFrame.java
+++ tank_war/src/main/java/pers/tz/tank/TankFrame.java
@@ -1,7 +1,9 @@
 package pers.tz.tank;
 
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
@@ -11,9 +13,11 @@ public class TankFrame extends Frame {
 	
 	Tank myTank = new Tank(200 ,200 ,Dir.UP);
 	Bullet b = new Bullet(300 ,400 ,Dir.UP);
+	private static final int GAME_WIDTH = 800 ,GAME_HEIGHT = 600;
+	Image offScreenImage = null ;
 	
 	public TankFrame(){
-		setSize(800 ,600);
+		setSize(GAME_WIDTH ,GAME_HEIGHT);
 		setResizable(false);
 		setTitle("tank war");
 		setVisible(true);
@@ -32,8 +36,26 @@ public class TankFrame extends Frame {
 	@Override
 	public void paint(Graphics g) {
 		//如果将tank的属性拿出来给别人画，就在一定程度上破坏了它的封装性
+		Color c = g.getColor();
+		g.setColor(Color.WHITE);
 		myTank.paint(g);
 		b.paint(g);
+	}
+	
+	
+	@Override
+	public void update(Graphics g) {
+		if (offScreenImage == null) {
+			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.BLACK);
+		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+		
 	}
 	
 	
