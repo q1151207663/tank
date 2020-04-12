55859e7fa72dd073fabf08c188ef882c76944b48
twz <1151207663@qq.com>
2020-04-12 14:14:08 +0800

    ???

--- tank_war/src/main/java/pers/tz/tank/TankFrame.java
+++ tank_war/src/main/java/pers/tz/tank/TankFrame.java
@@ -4,9 +4,10 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 public class TankFrame extends Frame {
+	
+	int x = 200 , y = 200;
 	
 	public TankFrame(){
 		setSize(800 ,600);
@@ -26,8 +27,10 @@ public class TankFrame extends Frame {
 	
 	@Override
 	public void paint(Graphics g) {
+		System.out.println("paint");
 		g.fillRect(200, 200, 50, 50);
-		
+		x += 10;
+		y += 10;
 	}
 	
 	
